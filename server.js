d91be38382affaef1210fd768f25728c51ab945f
Thanh Duy <ntd.codervn@gmail.com>
2020-02-07 12:24:32 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 const fs = require('fs');
 const pdf = require('pdf-parse');
